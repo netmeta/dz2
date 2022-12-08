e5f7b77f0fcfb33f1f471aa87027a58e075ddfac
netmeta <moron686@gmail.com>
2022-12-08 04:18:23 +0300

    first

---
+++ task1/Program.cs
@@ -0,0 +1,4 @@
+﻿int N = new Random().Next(100, 1000);
+Console.WriteLine(N);
+N = (N / 10) % 10;
+Console.WriteLine($"Вторая цифра числа -> {N}");
---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int N = new Random().Next(1, 65537);
+
+Console.Write(N);
+string stringNumber = Convert.ToString(N);
+
+if (stringNumber.Length > 2)
+{
+    Console.Write($" -> {stringNumber[2]}");
+}
+else
+{
+    Console.Write(" -> Третьей цифры нет");
+}
